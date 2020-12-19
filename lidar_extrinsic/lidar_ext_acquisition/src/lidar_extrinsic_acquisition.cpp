29b3b50b8296b7a8e5769a3e8e0874014fde9d3d
tes77 <tes77@case.edu>
2020-12-19 10:55:51 -0500

    Fixed training comma in position log.

--- lidar_extrinsic/lidar_ext_acquisition/src/lidar_extrinsic_acquisition.cpp
+++ lidar_extrinsic/lidar_ext_acquisition/src/lidar_extrinsic_acquisition.cpp
@@ -212,7 +212,11 @@ int main(int argc, char ** argv){
 
 					for(int r = 0; r < 4; r++){
 						for(int c = 0; c < 4; c++){
-							frame_output << std::to_string(m(r, c)) << ", ";
+							frame_output << std::to_string(m(r, c));
+							
+							if(c < 3 || r < 3){
+								frame_output << ", ";
+							}
 						}
 					}
 					frame_output << "\n";

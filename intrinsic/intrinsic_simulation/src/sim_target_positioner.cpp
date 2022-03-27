8c24f21f96babfc2a209aecfc26634b5f3e71e29
TomShkurti <tes77@case.edu>
2022-03-27 16:01:14 -0400

    Improve speed and stability of simulated target placement routine.

--- intrinsic/intrinsic_simulation/src/sim_target_positioner.cpp
+++ intrinsic/intrinsic_simulation/src/sim_target_positioner.cpp
@@ -3,13 +3,15 @@
 #include <std_msgs/Bool.h>
 #include <geometry_msgs/Point.h>
 #include <gazebo_msgs/DeleteModel.h>
+#include <gazebo_msgs/SetModelState.h>
 #include <turtlesim/Spawn.h>
 
 ros::Publisher success_publisher;
+ros::ServiceClient gz_client;
 bool target_move_CB(turtlesim::Spawn::Request & req, turtlesim::Spawn::Response & res){
 
 	//Delete the old target
-	gazebo_msgs::DeleteModel dm;
+	/*gazebo_msgs::DeleteModel dm;
 	dm.request.model_name = "target";
 	while(!ros::service::call("/gazebo/delete_model", dm) && ! ros::ok()){
 		ros::spinOnce();
@@ -22,7 +24,14 @@ bool target_move_CB(turtlesim::Spawn::Request & req, turtlesim::Spawn::Response 
 		+ "/xml/spawn_target.xml"
 		+ " x:=" + std::to_string(req.x) + " y:=" + std::to_string(req.y) + " z:=" + std::to_string(req.theta)
 	).c_str());
-	ros::Duration(1.0).sleep();
+	ros::Duration(1.0).sleep();*/
+	gazebo_msgs::SetModelState pos;
+	pos.request.model_state.pose.position.x = req.x;
+	pos.request.model_state.pose.position.y = req.y;
+	pos.request.model_state.pose.position.z = req.theta;
+	pos.request.model_state.model_name = "target";
+	
+	while(!gz_client.call(pos)){}
 
 	//Give the go-ahead to proceed
 	res.name = "";
@@ -35,6 +44,7 @@ int main(int argc, char ** argv){
 	ros::NodeHandle nh;
 	
 	ros::ServiceServer motion_maker = nh.advertiseService("/motion_command", target_move_CB);
+	gz_client = nh.serviceClient<gazebo_msgs::SetModelState>("/gazebo/set_model_state");
 	
 	ROS_INFO("Simulator is ready to move target.");
 	

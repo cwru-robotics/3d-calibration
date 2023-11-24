8377138c4f1ee233a495bd8d5f94b7488d731e70
tes77 <tes77@case.edu>
2023-11-24 12:49:19 -0500

    Wrong branch...

--- intrinsic/intrinsic_simulation/src/sim_target_positioner.cpp
+++ intrinsic/intrinsic_simulation/src/sim_target_positioner.cpp
@@ -8,11 +8,6 @@
 
 ros::Publisher success_publisher;
 ros::ServiceClient gz_client;
-double fRand(double fMin, double fMax){
-    double f = (double)rand() / RAND_MAX;
-    return fMin + f * (fMax - fMin);
-}
-
 bool target_move_CB(turtlesim::Spawn::Request & req, turtlesim::Spawn::Response & res){
 
 	//Delete the old target
@@ -34,22 +29,6 @@ bool target_move_CB(turtlesim::Spawn::Request & req, turtlesim::Spawn::Response 
 	pos.request.model_state.pose.position.x = req.x;
 	pos.request.model_state.pose.position.y = req.y;
 	pos.request.model_state.pose.position.z = req.theta;
-	
-	srand(req.x + req.y + req.theta);
-	double target_position_r = fRand(-20.0, 20.0) * 180.0 / M_PI;
-	double target_position_p = fRand(-20.0, 20.0) * 180.0 / M_PI;
-	double target_position_w = fRand(-30.0, 30.0) * 180.0 / M_PI;
-	
-	double cr = cos(target_position_r * 0.5);
-	double sr = sin(target_position_r * 0.5);
-	double cp = cos(target_position_p * 0.5);
-	double sp = sin(target_position_p * 0.5);
-	double cy = cos(target_position_w * 0.5);
-	double sy = sin(target_position_w * 0.5);
-	pos.request.model_state.pose.orientation.w = cr * cp * cy + sr * sp * sy;
-	pos.request.model_state.pose.orientation.x = sr * cp * cy - cr * sp * sy;
-	pos.request.model_state.pose.orientation.y = cr * sp * cy + sr * cp * sy;
-	pos.request.model_state.pose.orientation.z = cr * cp * sy - sr * sp * cy;
 	
 	pos.request.model_state.model_name = "target";
 	
